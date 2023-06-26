694e82660dd1cd98631cb50c32618fce97b042e3
SavaxTm <savax172@gmail.com>
2023-06-26 10:31:40 +0100

    Test files in

---
+++ test_files/_printf.c
@@ -0,0 +1,55 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "main.h"
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	char a;
+	const char *str;
+	va_list list;
+	va_start(list, format);
+
+	a = *format;
+
+	while (a != '\0')
+	{
+		format++;
+		if (a == '%')
+		{
+			a = *format;
+			format++;
+			if (a == 'c')
+			{
+				putchar(va_arg(list, int));
+				i++;
+			}
+			else if (a == 's')
+			{
+				str = va_arg(list, const char *);
+				while (*str != '\0')
+				{
+					putchar(*str);
+					str++;
+					i++;
+				}
+			}
+			else if (a == '%')
+			{
+				putchar('%');
+				i++;
+			}
+			else
+			{
+
+			}
+		}
+		else
+		{
+			putchar(a);
+			i++;
+		}
+		a = *format;
+	}
+	va_end(list);
+	return (i);
+}
